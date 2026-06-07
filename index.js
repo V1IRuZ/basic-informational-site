d6a76f76a0463d3e3c13414c749e457e29bf7cfa
Esa <esa-pitkanen@hotmail.com>
2026-06-07 15:52:10 +0300

    feat: apply styles.css to the pages
    - Add appropriate headers to files

--- index.js
+++ index.js
@@ -2,31 +2,47 @@ import http from "node:http";
 import fs from "node:fs";
 
 const server = http.createServer((req, res) => {
-  let path = "./";
 
-  switch (req.url) {
-    case "/":
-      path += "index.html"
-      break;
-    case "/about":
-      path += "about.html";
-      break;
-    case "/contact-me":
-      path += "contact-me.html";
-      break;
-    default:
-      path += "404.html";
-      break;
-  }
+  if (req.url === "/styles.css") {
+    fs.readFile("./styles.css", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      } else {
+        res.setHeader("Content-Type", "text/css");
+        res.end(data);
+      }
+    });
 
-  fs.readFile(path, "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.end();
-    } else {
-      res.end(data);
+  } else {
+
+    let path = "./";
+
+    switch (req.url) {
+      case "/":
+        path += "index.html";
+        break;
+      case "/about":
+        path += "about.html";
+        break;
+      case "/contact-me":
+        path += "contact-me.html";
+        break;
+      default:
+        path += "404.html";
+        break;
     }
-  });
+
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      } else {
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
+  }
 });
 
 server.listen(8080, "localhost", () => {
