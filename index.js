62d9ec94971b520245213ab4e956cdf775f388f7
Esa <esa-pitkanen@hotmail.com>
2026-06-07 12:45:09 +0300

    feat: add routing to all the pages
    - Change if statements to using switch in routing selection

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import http from "node:http";
 import fs from "node:fs";
 
-const index = "./index.html";
-
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(index, "utf-8", (err, data) => {
-      if (err) {
-        console.error(err);
-        res.end();
-      } else {
-        res.end(data);
-      }
-    });
-  } else if (req.url === "/about") {
-    res.end("about");
+  let path = "./";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html"
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      break;
+    default:
+      path += "404.html";
+      break;
   }
+
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(8080, "localhost", () => {
